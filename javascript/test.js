1b108cc2f01bcc99f3c9454d68ec0796de76fa6b
Senjai <r.crawfordwilson@gmail.com>
2013-07-14 12:35:47 -0700

    Treebook initial commit

--- javascript/test.js
+++ javascript/test.js
@@ -1,1 +1,7 @@
 // test.js Javascript testing file..
+
+
+var name = "Jim";
+console.log(name);
+var name2 = "This is a statement, he said something"
+console.log(name2);
---
+++ treebook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '61ee0fff5fe70b5b0ab96fc487efa2ca03d13fa777a3aeea4e6f0d6e0e801ec26f2c1e4a0da48480c022cdcd9638aa0b26550be33b9d52fa0ea0845dd7f91fe2'
