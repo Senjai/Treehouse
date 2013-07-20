8be3781df6443f9dbd371f3288f2d575d257b473
Senjai <r.crawfordwilson@gmail.com>
2013-07-19 21:54:06 -0700

    Updates

--- treebook/app/models/user.rb
+++ treebook/app/models/user.rb
@@ -8,6 +8,11 @@ class User < ActiveRecord::Base
 
   has_many :statuses
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true, uniqueness: true, format: { with: /a-zA-Z0-9_-/,
+                                                                       message: "Must be formatted correctly." }
+
   def full_name
     first_name + " " + last_name
   end
--- treebook/test/unit/user_test.rb
+++ treebook/test/unit/user_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user should enter their first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+    puts user.first_name
+  end
+
+  test "a user should enter their last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter their profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "the user should have a unique profile name" do
+    user = User.new
+    user.profile_name = users(:jason).profile_name
+    assert !user.save, "saved"
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a profile_name without spaces" do
+    user = User.new
+    user.profile_name = "My Profule with Spaces"
+
+    assert !user.save, "User was saved"
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
 end
