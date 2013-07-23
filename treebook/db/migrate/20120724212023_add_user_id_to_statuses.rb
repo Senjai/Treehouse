add395ab4093d7c5dba094e5070e78ae1dbeca40
Senjai <r.crawfordwilson@gmail.com>
2013-07-23 10:20:08 -0700

    Treebook isn't playing nicely..

--- treebook/db/migrate/20120724212023_add_user_id_to_statuses.rb
+++
@@ -1,7 +0,0 @@
-class AddUserIdToStatuses < ActiveRecord::Migration
-  def change
-    add_column :statuses, :user_id, :integer
-    add_index :statuses, :user_id
-    remove_column :statuses, :name
-  end
-end
