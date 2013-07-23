0de135fd21dc92aae13bd3fffdde7c5b529fb5b3
Senjai <r.crawfordwilson@gmail.com>
2013-07-23 11:05:57 -0700

    Hopefully this is fixed (modern Gemfile with cloned repository)

---
+++ treebook/app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :first_name, :last_name, :profile_name
+  
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
+
+  validates :profile_name, presence: true,
+                           uniqueness: true,
+                           format: {
+                             with: /[a-zA-Z0-9_-]+/,
+                             message: 'Must be formatted correctly.'
+                           }
+
+  has_many :statuses
+
+  def full_name
+  	first_name + " " + last_name
+  end
+end
---
+++ treebook/test/functional/statuses_controller_test.rb
@@ -0,0 +1,79 @@
+require 'test_helper'
+
+class StatusesControllerTest < ActionController::TestCase
+  setup do
+    @status = statuses(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:statuses)
+  end
+
+  test "should be redirected when not logged in" do
+    get :new
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should render the new page when logged in" do
+    sign_in users(:jason)
+    get :new
+    assert_response :success
+  end
+
+  test "should be logged in to post a status" do
+    post :create, status: { content: "Hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:jason)
+
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content }
+    end
+
+    assert_equal users(:jason), assigns(:status).user
+    assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should show status" do
+    get :show, id: @status
+    assert_response :success
+  end
+
+  test "should redirect edit when not logged in" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get edit when logged in" do
+    sign_in users(:jason)
+    get :edit, id: @status
+    assert_response :success
+  end
+
+  test "should redirect status update when not logged in" do
+    put :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update status when logged in" do
+    sign_in users(:jason)
+    put :update, id: @status, status: { content: @status.content }
+    assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should destroy status" do
+    assert_difference('Status.count', -1) do
+      delete :destroy, id: @status
+    end
+
+    assert_redirected_to statuses_path
+  end
+end
