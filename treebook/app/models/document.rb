220b4184fc9eba7f122b7517d41c2dc2bbcaa9b7
Senjai <r.crawfordwilson@gmail.com>
2013-08-11 21:09:13 -0700

    Basic association setup and form inclusion

--- treebook/app/models/document.rb
+++ treebook/app/models/document.rb
@@ -1,3 +1,6 @@
 class Document < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :attachment
+  has_attached_file :attachment
+
+  belongs_to :user
 end
--- treebook/app/models/status.rb
+++ treebook/app/models/status.rb
@@ -1,6 +1,9 @@
 class Status < ActiveRecord::Base
-  attr_accessible :content, :user_id
+  attr_accessible :content, :user_id, :document_attributes
   belongs_to :user
+  belongs_to :document
+
+  accepts_nested_attributes_for :document
 
   validates :content, presence: true,
                       length: { minimum: 2 }
